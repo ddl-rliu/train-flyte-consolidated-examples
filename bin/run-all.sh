6624cb0fa3e0faf4327051fe8ce929899aa1568e
ddl-rliu <richard.liu@dominodatalab.com>
2024-09-17 14:28:18 -0700

    x

--- bin/run-all.sh
+++ bin/run-all.sh
@@ -1,7 +1,8 @@
 TFCE_WORKFLOWS="./train-flyte-consolidated-examples/workflows"
 TFCE_RUN="pyflyte run --remote"
-$TFCE_RUN $TFCE_WORKFLOWS/artifacts.py wf
+$TFCE_RUN $TFCE_WORKFLOWS/artifacts_workflow.py wf
+$TFCE_RUN $TFCE_WORKFLOWS/caching_workflow.py wf
+$TFCE_RUN $TFCE_WORKFLOWS/inputs_complex_workflow.py wf
 $TFCE_RUN $TFCE_WORKFLOWS/inputs_rare_workflow.py wf
-$TFCE_RUN $TFCE_WORKFLOWS/inputs_workflow.py wf
+$TFCE_RUN $TFCE_WORKFLOWS/inputs_unions_workflow.py wf
 $TFCE_RUN $TFCE_WORKFLOWS/nested_workflow.py wf
-$TFCE_RUN $TFCE_WORKFLOWS/unions_workflow.py wf
